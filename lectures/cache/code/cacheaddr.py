a64963c3334dba11e402fa63d7d54bc5be8bd1c6
Pat Hanrahan <hanrahan@cs.stanford.edu>
2016-02-29 11:55:49 -0800

    Don't print out value.

--- lectures/cache/code/cacheaddr.py
+++ lectures/cache/code/cacheaddr.py
@@ -1,7 +1,7 @@
 from random import randint
 
-print " A  M S L"
+print " A  S L"
 for i in range(16):
     addr = randint(0,255)
     cacheaddr = addr&0x7
-    print "%02X %02X %X %X" % (addr, 255-addr, (cacheaddr>>1), (cacheaddr&1))
+    print "%02X %X %X" % (addr, (cacheaddr>>1), (cacheaddr&1))
