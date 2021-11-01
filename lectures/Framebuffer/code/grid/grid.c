12a0f014ae0309c9e31e08c3ab0a737a24b5155a
Philip Levis <pal@cs.stanford.edu>
2021-11-01 04:25:37 +0000

    Space the grid out a bit more, and don't call functions within loops. commit dca73874de60f6344a8bc81a821e612477054883 Author: Philip Levis <pal@cs.stanford.edu> Date:   Sun Oct 31 21:20:10 2021 -0700
        Space the grid out a bit more, and don't call functions within
        loops.

--- lectures/Framebuffer/code/grid/grid.c
+++ lectures/Framebuffer/code/grid/grid.c
@@ -7,23 +7,25 @@ static const int WIDTH = 640;
 static const int HEIGHT = 512;
 static const int DEPTH = 4;
 
-void draw_pixel(int x, int y, color_t c)
-{
-    unsigned int per_row = fb_get_pitch()/DEPTH; // length of each row in pixels (include any padding)
-    unsigned int (*im)[per_row] = fb_get_draw_buffer();
-    im[y][x] = c;
+void draw_pixel(int x, int y, color_t c) {
+	unsigned int row_width = fb_get_pitch()/DEPTH;
+	unsigned int (*screen)[row_width] = fb_get_draw_buffer();
+	screen[y][x] = c;
 }
 
-void draw_hline(int y, color_t c)
-{
-    for( int x = 0; x < fb_get_width(); x++ )
+void draw_hline(int y, color_t c) {
+	int width = fb_get_width();
+    for(int x = 0; x < width; x++ ) {
         draw_pixel(x, y, c);
+	}
 }
 
 void draw_vline(int x, color_t c)
 {
-    for( int y = 0; y < fb_get_height(); y++ )
-        draw_pixel(x, y, c);
+	int height = fb_get_height();
+	for (int y = 0; y < height; y++) {
+		draw_pixel(x, y, c);
+	}
 }
 
 
@@ -31,10 +33,12 @@ void main(void)
 {
     fb_init(WIDTH, HEIGHT, DEPTH, FB_SINGLEBUFFER);
 
-    for( int y = 0; y < HEIGHT; y += 16 )
-        draw_hline(y, WHITE);
+	for (int y = 0; y < HEIGHT; y += 32) {
+		draw_hline(y, WHITE);
+	}
 
-    for( int x = 0; x < WIDTH; x += 16 )
-        draw_vline(x, WHITE);
+	for (int x = 0; x < WIDTH; x += 32) {
+		draw_vline(x, WHITE);
+	}
 }
 
