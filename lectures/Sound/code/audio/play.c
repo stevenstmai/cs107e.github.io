80317481d3d19364fdb18be7f4bc5c295b8fbe0f
Chris Gregg <tofergregg@gmail.com>
2021-11-05 20:26:16 +0000

    released interrupts 1 lecture commit aaa1f6aacf8da4e48ed011ef746a78bf394363c1 Author: Chris Gregg <tofergregg@gmail.com> Date:   Fri Nov 5 13:20:57 2021 -0700
        released interrupts 1 lecture

--- lectures/Sound/code/audio/play.c
+++ lectures/Sound/code/audio/play.c
@@ -3,9 +3,25 @@
 #include "audio.h"
 #include "printf.h"
 #include "timer.h"
-// #include "sin8.h"
+#include "sin8.h"
 // #include "sound2.h"
-#include "sound3.h"
+// #include "sound3.h"
+
+struct wav_format {
+   uint32_t description; // should be 'RIFF'
+   uint32_t filesize; // little endian
+   uint32_t wav_header; // should be 'WAVE'
+   uint32_t wav_format; // should be 'fmt '
+   uint32_t type_format_size; // should be 16, as in the above four fields are 16 bytes 
+   uint16_t wav_type; // 0x01 is PCM
+   uint16_t num_channels; // 0x01 is mono, 0x02 is stereo, etc.
+   uint32_t sample_rate; // e.g., 44100, little-endian
+   uint32_t rate_plus; // sample rate * bits per sample * channels / 8
+   uint16_t bits_per_sample; // e.g., 16
+   uint32_t data_mark; // should be 'data'
+   uint32_t data_size; // little endian
+   uint8_t *data; // may be 16-bit
+};
 
 void main ()
 {
