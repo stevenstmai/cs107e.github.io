22e07d525cb153e65086fc770e260589718f577b
Philip Levis <pal@cs.stanford.edu>
2021-11-07 21:20:15 +0000

    Add updated reference code. commit f2c9c0c422d438fbb0d2eb799c55b9e3c751cf0c Author: Philip Levis <pal@cs.stanford.edu> Date:   Sun Nov 7 13:15:03 2021 -0800
        Add updated reference code.

--- cs107e/include/ref/interrupts_ref.h
+++ cs107e/include/ref/interrupts_ref.h
@@ -3,4 +3,6 @@
 void ref_interrupts_init(void);
 void ref_interrupts_global_enable(void);
 void ref_interrupts_global_disable(void);
+void ref_interrupts_enable_source(unsigned int source);
+void ref_interrupts_disable_source(unsigned int source);
 void ref_interrupts_register_handler(unsigned int source, handler_fn_t fn, void *aux_data);
