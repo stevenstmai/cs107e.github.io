e8b86db088f64eca2a462bb84c2d19bcf824f71a
Julie Zelenski <zelenski@cs.stanford.edu>
2022-02-04 17:59:48 +0000

    gdb example with stack corruption commit c3488ccafa02e9a2d347d78ce134983d043b9d5c Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Fri Feb 4 09:53:46 2022 -0800
        gdb example with stack corruption

--- lectures/C_Mastery/code/debugger/program.c
+++ lectures/C_Mastery/code/debugger/program.c
@@ -2,16 +2,21 @@
 #include "timer.h"
 #include "printf.h"
 #include "uart.h"
+#include "strings.h"
 
 void blink_gpio(unsigned int pin)
 {
+    uart_putstring("Watch me blink the green led on the Pi\n");
     gpio_set_output(pin);
     for (int i = 0; i < 3; i++) {
+        uart_putstring("on...");
         gpio_write(pin, 1);
         timer_delay(1);
+        uart_putstring("off...");
         gpio_write(pin, 0);
         timer_delay(1);
     }
+    uart_putstring("Done\n");
 }
 
 int increment_if(int n)
@@ -21,10 +26,22 @@ int increment_if(int n)
     return n;
 }
 
+bool trouble(int howmany)
+{
+    char buf[5] = "abcd";
+    printf("How much trouble? Writing %d bytes beyond... ", howmany);
+    memset(&buf[4], 0, howmany);
+    return true;
+}
+
 void main(void)
 {
+    static int i;
+
     gpio_init();
     uart_init();
+
+    printf("\n\n++++++++++ Starting program.c ++++++++++\n");
 
     blink_gpio(GPIO_PIN47);
 
@@ -33,5 +50,9 @@ void main(void)
     result = increment_if(0);
     printf("Increment(0) = %d\n", result);
 
-    uart_putchar(EOT);
+    for (i = 0; i < 100; i += 4) {
+        if (trouble(i))
+            printf("success!\n");
+    }
+    printf("Bye now!\n\004");
 }
