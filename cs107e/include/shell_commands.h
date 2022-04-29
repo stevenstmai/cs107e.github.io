38bd123c78610f45ad986de71b91b71f5e808ca4
Julie Zelenski <zelenski@cs.stanford.edu>
2022-04-29 01:35:30 +0000

    Review all output from shell commands, make consistent across uses. commit 703f5a15fbcf6013ade4e215c07bfe6a0a744ebe Author: Julie Zelenski <zelenski@cs.stanford.edu> Date:   Thu Apr 28 18:26:41 2022 -0700
        Review all output from shell commands, make consistent across uses.
    
        Ensure that reference code, shell_commands.h header, and assign writeup
        all agree. Confirm with library unit tests and assignment test staff logs.
        Starter code will refer students to shell_commands.h header.

--- cs107e/include/shell_commands.h
+++ cs107e/include/shell_commands.h
@@ -10,11 +10,12 @@ typedef int (*command_fn_t)(int argc, const char *argv[]);
 
 /*
  * This typedef defines the type for each entry in the command table.
- * A command_t stores the info for one command, including one-word name,
- * help description, and function pointer to execute the command.
+ * A command_t stores the info for one command, including the one-word name,
+ * usage, help description, and function pointer to execute the command.
  */
 typedef struct _command_struct {
     const char *name;
+    const char *usage;
     const char *description;
     command_fn_t fn;
 } command_t;
@@ -22,96 +23,108 @@ typedef struct _command_struct {
 /*
  * `cmd_echo`
  *
- * Prints its arguments and returns 0.
+ * Usage: echo [args]
+ * Description: print arguments
+ *
+ * Example:
+ *   Pi> echo Leland Stanford Junior University
+ *   Leland Stanford Junior University
+ *
+ * Print arguments. Returns 0 (success).
 */
 int cmd_echo(int argc, const char *argv[]);
 
 /*
  * `cmd_help`
  *
- * When called without arguments:
- *   Prints a list of all available commands and their descriptions.
+ * Usage: help [cmd]
+ * Description: print command usage and description
  *
- * Example:
+ * If no argument, print a list of all available commands with
+ * usage and description. If an argument is given, print
+ * usage and description for named command. If no such
+ * named command, prints an error message.
+ *
+ * Examples:
  *   Pi> help
- *   echo:   echo user input to the screen
- *   reboot: reboot the Raspberry Pi back to the bootloader
- *   ...
- *
- * When called with one argument
- *   Prints the description for that command, or an error message if that
- * command does exist:
- *
+ *   help [cmd]         print command usage and description
+ *   echo [args]        print arguments
+ *   reboot             reboot the Raspberry Pi
+ *   peek [addr]        print contents of memory at address
+ *   poke [addr] [val]  store value into memory at address
  *   Pi> help reboot
- *   reboot:  reboot the Raspberry Pi back to the bootloader
+ *   reboot             reboot the Raspberry Pi
  *   Pi> help please
- *   error: no such command `please`.
+ *   error: no such command 'please'
  *
  * Ignores any arguments after the first. Returns 0 on success,
- * 1 on error (command not found).
+ * nonzero 1on error (command not found).
  */
 int cmd_help(int argc, const char *argv[]);
 
 /*
  * `cmd_reboot`
  *
- * reboot the Raspberry Pi back to the bootloader using `pi_reboot`
+ * Usage: reboot
+ * Description: reboot the Raspberry Pi
  *
- * Ignores all arguments. This function does not return; the program
- * exits and Pi is restarted.
+ * Example:
+ *   Pi> reboot
+ *   Rebooting Pi, see ya at the bootloader!
+ *
+ * Ignores all arguments. Prints informative message and reboots Pi.
+ * This function does not return; the program exits on reboot.
  */
 int cmd_reboot(int argc, const char *argv[]);
 
 /*
  * `cmd_peek`
  *
- * Usage: peek [addresss]
+ * Usage: peek [addr]
+ * Description: print contents of memory at address
  *
- * Prints the contents (4 bytes) of memory at address.
- * If address argument is prefixed with 0x, it is interpreted as hex,
+ * Retrieve 4-byte value at memory address and print it.
+ * If address is prefixed with 0x, it is interpreted as hex,
  * otherwise interpreted as decimal. Address must be 4-byte aligned.
+ * If address is missing or invalid, prints an error message.
  *
- * Example:
- *
+ * Examples:
  *   Pi> peek 0
  *   0x00000000:  e59ff030
- *
- * If the address argument is missing or invalid, prints an error message:
- *
  *   Pi> peek
- *   error: peek requires 1 argument [address]
+ *   error: peek expects 1 argument [addr]
  *   Pi> peek fred
  *   error: peek cannot convert 'fred'
+ *   Pi> peek 13
+ *   error: peek address must be 4-byte aligned
  *
- * Ignores any arguments after the first. Returns 0 on success, 1 on error.
+ * Ignores any arguments after the first. Returns 0 on success, nonzero on error.
  */
 int cmd_peek(int argc, const char *argv[]);
 
 /*
  * `cmd_poke`
  *
- * Usage: poke [adress] [value]
+ * Usage: poke [addr] [val]
+ * Description: store value into memory at address
  *
- * Stores `value` into the memory at `address`.
- * If a numeric argument is prefixed with 0x, it is interpreted as hex,
+ * Writes 4-byte value to memory at address.
+ * If address or value is prefixed with 0x, it is interpreted as hex,
  * otherwise interpreted as decimal. Address must be 4-byte aligned.
+ * If address or value is missing or invalid, prints an error message.
  *
- * Example:
- *
- *   Pi> poke 0x1000 3
- *   Pi> peek 0x1000
- *   0x00001000: 00000003
- *
- * If argument is missing or invalid, prints an error message:
- *
- *   Pi> poke 0xFFFC
- *   error: poke requires 2 arguments [address] and [value]
+ * Examples:
+ *   Pi> poke 0x8000 3
+ *   Pi> peek 0x8000
+ *   0x00008000:  00000003
+ *   Pi> poke 0x8000
+ *   error: poke expects 2 arguments [addr] and [val]
  *   Pi> poke bob 3
  *   error: poke cannot convert 'bob'
  *   Pi> poke 11 0
  *   error: poke address must be 4-byte aligned
  *
- * Ignores any arguments after the second. Returns 0 on success, 1 on error.
+ * Ignores any arguments after the second. Returns 0 on success, nonzero on error.
  */
 int cmd_poke(int argc, const char *argv[]);
 
