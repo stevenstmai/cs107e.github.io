71209de5c7a2ea9d9d89517cb762b5ff1de27a0a
Chris Gregg <tofergregg@gmail.com>
2022-09-30 20:27:44 +0000

    udpated ARM lecture commit eaea0912e93ed747a4576a357e9b2f7580e9263e Author: Chris Gregg <tofergregg@gmail.com> Date:   Fri Sep 30 13:22:33 2022 -0700
        udpated ARM lecture

--- lectures/ARM/code/on/on.s
+++ lectures/ARM/code/on/on.s
@@ -5,11 +5,10 @@
 // GPIO 20 function select is in the 2nd function select register
 //
 
-mov r0, #0x20      // #0x00000020
-lsl r1, r0, #24    // #0x20000000
-lsl r2, r0, #16    // #0x00200000
-orr r0, r1, r2     // #0x20200000
-orr r0, r0, #0x08  // #0x20200008
+# FSEL2= 0x20200008
+mov r0, #0x20000000
+orr r0, #0x00200000
+orr r0, #0x00000008
 mov r1, #1         // indicates OUTPUT
 str r1, [r0]       // store 1 to address 0x20200008
 
